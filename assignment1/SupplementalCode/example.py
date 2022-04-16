aa4904fab431a3740a1823d1360b6e4d1d693082
Irene Papadopoulou <irenepap3@gmail.com>
2022-04-16 20:40:19 +0200

    Code for part 3.

--- assignment1/SupplementalCode/example.py
+++ assignment1/SupplementalCode/example.py
@@ -5,18 +5,14 @@ from scipy.spatial.distance import cdist
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
 from scipy.spatial import cKDTree
+from random import sample
 
 # globals.
 DATA_DIR = 'Data'  # This depends on where this file is located. Change for your needs.
-
-
-######                                                           ######
-##      notice: This is just some example, feel free to adapt        ##
-######                                                           ######
-
+FRAMES_PATH = DATA_DIR + '/data/'
 
 # == Load data ==
-def open3d_example(path="Data/data/0000000000.pcd"):
+def get_cleaned_pointcloud(path):
     pcd = o3d.io.read_point_cloud(path)
     
     # convert into ndarray
@@ -26,10 +22,7 @@ def open3d_example(path="Data/data/0000000000.pcd"):
     dist = np.sqrt(np.sum(pcd_arr ** 2, axis = 1))
     pcd_arr_cleaned = pcd_arr[dist < 2]
 
-    # visualization from ndarray
-    vis_pcd = o3d.geometry.PointCloud()
-    vis_pcd.points = o3d.utility.Vector3dVector(pcd_arr_cleaned)
-    o3d.visualization.draw_geometries([vis_pcd])
+    return pcd_arr_cleaned
 
 
 def open_wave_data():
@@ -100,6 +93,7 @@ def get_new_R_t(source, target):
 
     return R, t
 
+
 ############################
 #     ICP                  #
 ############################
@@ -113,7 +107,7 @@ def icp(A1, A2, max_iterations=20, epsilon=0.01, kd_tree=False):
     t = translation = np.zeros((3,1))
     source = A1
 
-    past_rms = np.inf
+    past_rms = 1
 
     for iter in range(max_iterations):
         # go to 2. unless RMS is unchanged(<= epsilon)
@@ -122,7 +116,6 @@ def icp(A1, A2, max_iterations=20, epsilon=0.01, kd_tree=False):
         
         # 3. transform point cloud with R and t
         A1 = R @ A1 + t
-        # plot_progress(source, A2, A1, iter)
 
         # 4. Find the closest point for each point in A1 based on A2 using brute-force approach
         if kd_tree:
@@ -180,21 +173,109 @@ def plot_progress(source, target, trans, iter, dir='./figures/waves', save_figur
 #   Merge Scene            #
 ############################
 
-#  Estimate the camera poses using two consecutive frames of given data.
+def get_frame_pointclouds(step=1, frame_count=100):
+    '''  
+    Returns frame pointclouds depending on step size
+    step = N = {1,2,4,10}
+    '''
 
-#  Estimate the camera pose and merge the results using every 2nd, 4th, and 10th frames.
+    frames = []
+ 
+    counts = ["{0:02}".format(i) for i in range(0, frame_count, step)]
+    filename_paths = [FRAMES_PATH + f'00000000{i}.pcd' for i in counts]
 
-#  Iteratively merge and estimate the camera poses for the consecutive frames.
+    for file_path in filename_paths:
+        frames.append(get_cleaned_pointcloud(file_path).T)
+
+    return frames
+
+
+def estimate_transformations(frame_pcds):
+    
+    #  Estimate the camera poses using two consecutive frames of given data.
+    rotations = []
+    translations = []
+    for i in range(len(frame_pcds)-1):
+        print('Frame ', i+1)
+        R, t = icp(frame_pcds[i], frame_pcds[i+1], kd_tree=True, epsilon=1e-4, max_iterations=50)
+        rotations.append(R)
+        translations.append(t)
+
+    # #Estimating rotation and translation from last frame to first
+    # print('Frame ', len(frame_pcds)-1)
+    # R, t = icp(frame_pcds[len(frame_pcds)-1], frame_pcds[0], kd_tree=True, epsilon=1e-4, max_iterations=50)
+    # rotations.append(R)
+    # translations.append(t)
+    
+    return rotations, translations
 
 
 ############################
 #  Additional Improvements #
 ############################
 
+def subsample_graph(A1, points=1000):
+    '''
+    Create reduced point cloud for subsampling
+    '''
+    gen_points = sample(range(0, len(A1[0] - 1)), points)
+    new_A1 = np.array([[], [], []])
+
+    # Create downsampled A1 from generated points
+    for point in gen_points:
+        new_point = np.array([[A1[0][point]], [A1[1][point]], [A1[2][point]]])
+        new_A1 = np.hstack((new_A1, new_point))
+
+    return np.array(new_A1)
+
+
+def merge_scene(merge_in_between=False, frame_count=100, step=1, visualize=True):
+    '''
+    Estimates the camera poses of every N frames and merges the results
+    (either in between every ipc calculation, or all together at the end)
+    '''
+
+    frame_pcds = get_frame_pointclouds(step=step, frame_count=frame_count) # step = N = {1, 2, 4, 10}
+
+    #  Iteratively merge and estimate the camera poses for the consecutive frames.
+    if merge_in_between:
+        source_frame = frame_pcds[0]
+        for i, frame in enumerate(frame_pcds[1:]):
+            print("Frame:", i+1)
+            R, t = icp(source_frame, frame, kd_tree=True, epsilon=1e-2, max_iterations=10)
+            new_source = R @ source_frame + t
+            source_frame = np.hstack((new_source, frame))
+        
+        trans = subsample_graph(source_frame, points=20000)
+        trans_pcd = o3d.geometry.PointCloud()
+        trans_pcd.points = o3d.utility.Vector3dVector(trans.T)
+        trans_pcds = [trans_pcd]
+
+    # Estimate the camera poses for the consecutive frames and merge at the end
+    else:
+        rotations, translations = estimate_transformations(frame_pcds) 
+        
+        trans_pcds = []
+        for i, frame in enumerate(frame_pcds):
+            r_list = rotations[i:]
+            t_list = translations[i:]
+            for r, t in zip(r_list, t_list):
+                frame = r @ frame + t
+
+            trans = subsample_graph(frame)
+            trans_pcd = o3d.geometry.PointCloud()
+            trans_pcd.points = o3d.utility.Vector3dVector(trans.T)
+            trans_pcds.append(trans_pcd)
+
+    if visualize:
+        o3d.visualization.draw_geometries(trans_pcds)
+
 if __name__ == "__main__":
-    # open3d_example()
-    # source, target = open_wave_data()
-    source, target = open_bunny_data()
-    R, t = icp(source, target, kd_tree=True, epsilon=1e-8, max_iterations=50)
-    trans = (R @ source) + t
-    plot_progress(source, target, trans, iter='last', dir='./figures/bunny', save_figure=True)
+
+    merge_scene()
+
+    # source, target = open_bunny_data()
+    # # source, target = open_wave_data()
+    # R, t = icp(source, target, kd_tree=True, epsilon=1e-8, max_iterations=50)
+    # trans = (R @ source) + t
+    # plot_progress(source, target, trans, iter='last', dir='./figures/bunny', save_figure=True)
