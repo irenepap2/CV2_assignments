32c36819f9f3253f83611a89a24027c360db496b
Gijsvanmeer <59614500+Gijsvanmeer@users.noreply.github.com>
2022-04-22 15:20:18 +0200

    FInally fixed noise function testing

--- assignment1/SupplementalCode/experiments.py
+++ assignment1/SupplementalCode/experiments.py
@@ -1,4 +1,5 @@
-from icp import *
+from icp import icp
+from utils import *
 import time
 import numpy as np
 
@@ -24,40 +25,41 @@ def obtain_avgs(array_set):
     return averages, np.mean(lengths)
 
 
-def write_results(source, target, sample, f, g, ratio=None, noise=False, noise_max=0):
+def icp_results(source, target, sample, ratio=None, noise=False, noise_max=0):
     '''
-    Obtain average ICP results and write them to file.
+    Obtain average ICP results.
     '''
     results = []
     times = []
 
-    for i in range(25):
+    for i in range(1):
         time1 = time.time()
 
         if not ratio:
-            _, _, rms = icp(source, target, sampling=sample, epsilon=1e-8, max_iters=100, ratio=0.01, mode='kd_tree', print_rms=False, noise=noise, noise_max=noise_max)
+            _, _, rms = icp(source, target, sampling=sample, epsilon=1e-8, max_iters=100, ratio=0.01, kd_tree=True, print_rms=False, noise=noise, noise_max=noise_max)
         else:
-            _, _, rms = icp(source, target, sampling=sample, epsilon=1e-8, max_iters=100, ratio=ratio, mode='kd_tree', print_rms=False)
+            _, _, rms = icp(source, target, sampling=sample, epsilon=1e-8, max_iters=100, ratio=ratio, kd_tree=True, print_rms=False)
 
         time2 = time.time()
         results.append(rms)
         times.append(time2-time1)
 
-    f.write('Average time: ' + str(np.mean(times)) + '\n')
-    g.write('Total times: ' + str(times) + '\n')
     averages, mean_l = obtain_avgs(results)
-    f.write('Average length: ' + str(mean_l) + '\n')
-    f.write('Average rms: ' + str(averages) + '\n')
-    g.write('Total rms: ' + str(results) + '\n')
 
-    return
+    return results, times, np.mean(times), averages, mean_l
 
 
-def obtain_results(source, target, samplings, noise=False, mode='kd_tree'):
+def obtain_results(source, target, samplings, noise=False, z_buff=False):
     '''
     Obtain results and write them to file
     '''
     for sampling in samplings:
+        total_res = []
+        total_times = []
+        total_mean_times = []
+        total_avgs = []
+        total_mean_l = []
+        print(sampling)
         file = str(sampling) + '.txt'
 
         if noise:
@@ -66,48 +68,99 @@ def obtain_results(source, target, samplings, noise=False, mode='kd_tree'):
         if z_buff:
             file = "z_buff_" + file
 
-        with open(DATA_DIR + file, 'w') as f:
-            with open(DATA_DIR + 'raw' + file, 'w') as g:
+        # obtain noised results
+        if noise:
+            noise_max = [0.1, 0.25, 0.5, 0.75]
 
-                # obtain noised results
-                if noise:
-                    noise_max = [0.1, 0.25, 0.5, 0.75]
+            for noise_v in noise_max:
+                results, times, mean_times, averages, mean_l = icp_results(source, target, sampling, noise=noise, noise_max=noise_v)
+                total_res.append(results)
+                total_times.append(times)
+                total_mean_times.append(mean_times)
+                total_avgs.append(averages)
+                total_mean_l.append(mean_l)
 
-                    for noise_v in noise_max:
-                        f.write("Noise: " + str(noise_v) +'\n')
-                        g.write("Noise: " + str(noise_v) +'\n')
-                        write_results(source, target, sampling, f, g, noise, noise_v)
-                        f.write('\n')
-                        g.write('\n')
+        # obtain results of non random/uniform subsampling
+        elif sampling != 'uniform' and sampling != 'random':
+            results, times, mean_times, averages, mean_l = icp_results(source, target, sampling)
+            total_res.append(results)
+            total_times.append(times)
+            total_mean_times.append(mean_times)
+            total_avgs.append(averages)
+            total_mean_l.append(mean_l)
 
-                # obtain results of non random/uniform subsampling
-                elif sampling != 'uniform' and sampling != 'random':
-                    write_results(source, target, sampling, f, g)
+        # obtain subsampling results with different ratios
+        else:
+            ratios = [0.01, 0.1, 0.25, 0.5, 0.75]
+            for ratio in ratios:
+                results, times, mean_times, averages, mean_l = icp_results(source, target, sampling, ratio)
+                total_res.append(results)
+                total_times.append(times)
+                total_mean_times.append(mean_times)
+                total_avgs.append(averages)
+                total_mean_l.append(mean_l)
+        result_l = [total_res, total_times, total_mean_times, total_avgs, total_mean_l]
 
-                # obtain subsampling results with different ratios
-                else:
-                    ratios = [0.01, 0.1, 0.25, 0.5, 0.75]
-                    for ratio in ratios:
-                        f.write("Ratio: " + str(ratio) + '\n')
-                        g.write("Ratio: " + str(ratio) + '\n')
-                        write_results(source, target, sampling, f, g, ratio)
-                        f.write('\n')
-                        g.write('\n')
+        write_files(file, sampling, result_l, noise)
 
-                g.close()
 
-            f.close()
+def write_files(file, sampling, result_l, noise):
+    '''
+    Write results to file.
+    '''
+    with open(DATA_DIR + file, 'w') as f:
+        with open(DATA_DIR + 'raw' + file, 'w') as g:
+
+            # obtain noised results
+            if noise:
+                noise_max = [0.1, 0.25, 0.5, 0.75]
+
+                for i, noise_v in enumerate(noise_max):
+                    f.write("Noise: " + str(noise_v) +'\n')
+                    g.write("Noise: " + str(noise_v) +'\n')
+                    f.write('Average time: ' + str(result_l[2][i]) + '\n')
+                    g.write('Total times: ' + str(result_l[1][i]) + '\n')
+                    f.write('Average length: ' + str(result_l[4][i]) + '\n')
+                    f.write('Average rms: ' + str(result_l[3][i]) + '\n')
+                    g.write('Total rms: ' + str(result_l[0][i]) + '\n')
+                    f.write('\n')
+                    g.write('\n')
+
+            # obtain results of non random/uniform subsampling
+            elif sampling != 'uniform' and sampling != 'random':
+                f.write('Average time: ' + str(result_l[2][0]) + '\n')
+                g.write('Total times: ' + str(result_l[1][0]) + '\n')
+                f.write('Average length: ' + str(result_l[4][0]) + '\n')
+                f.write('Average rms: ' + str(result_l[3][0]) + '\n')
+                g.write('Total rms: ' + str(result_l[0][0]) + '\n')
+
+            # obtain subsampling results with different ratios
+            else:
+                ratios = [0.01, 0.1, 0.25, 0.5, 0.75]
+                for ratio in ratios:
+                    f.write("Ratio: " + str(ratio) + '\n')
+                    g.write("Ratio: " + str(ratio) + '\n')
+                    f.write('Average time: ' + str(result_l[2][i]) + '\n')
+                    g.write('Total times: ' + str(result_l[1][i]) + '\n')
+                    f.write('Average length: ' + str(result_l[4][i]) + '\n')
+                    f.write('Average rms: ' + str(result_l[3][i]) + '\n')
+                    g.write('Total rms: ' + str(result_l[0][i]) + '\n')
+                    f.write('\n')
+                    g.write('\n')
+
+            g.close()
+
+        f.close()
 
 
 if __name__ == "__main__":
     '''
     Use this to generate the data.
-    WARNING: Data generation will take close to 12 hours in total.
-    If you want to reduce this dont run the noised versions (especially of
-    random sampling).
+    WARNING: Data generation may take a while.
+    Uncomment any of the 2 methods to obtain the results and save to text file.
     '''
-    # source, target, _ = open_wave_data()
-    source, target, _ = open_bunny_data()
-    # samplings = ['uniform', 'random', 'multi_res', 'info_reg', 'none']
+    # source, target = open_wave_data()
+    source, target = open_bunny_data()
+    samplings = ['uniform', 'random', 'multi_res', 'info_reg', 'none']
     # obtain_results(source, target, samplings, noise=False)
     # obtain_results(source, target, samplings, noise=True)
