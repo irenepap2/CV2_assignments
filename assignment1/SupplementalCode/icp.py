e9eff89f9f75b5fa16af0540b9c7ae6d13ebf11b
MAStrampel <59614509+MAStrampel@users.noreply.github.com>
2022-04-22 13:08:23 +0200

    Update icp.py
    Added z_buffer, cleaned some things

--- assignment1/SupplementalCode/icp.py
+++ assignment1/SupplementalCode/icp.py
@@ -9,9 +9,9 @@ from utils import *
 ############################
 
 def calculate_closest_points(A1, A2):
-    '''  
-    Computes euclidean distance from every source pointcloud A1 
-    to every target pointcloud A2 and returns the corresponding A2 matches 
+    '''
+    Computes euclidean distance from every source pointcloud A1
+    to every target pointcloud A2 and returns the corresponding A2 matches
     with the minimum distance from the A1 cloudpoints
     '''
 
@@ -23,15 +23,98 @@ def calculate_closest_points(A1, A2):
 
 
 def calculate_closest_points_kd_tree(A1, A2):
-    '''  
+    '''
     Computes closest points for every source pointcloud A1 using the kd-tree
     '''
 
     tree = cKDTree(A2.T)
-    _, shortest_indx = tree.query(A1.T) 
+    _, shortest_indx = tree.query(A1.T)
     matches = A2[:, shortest_indx]
 
     return matches
+
+
+def calculate_closest_points_z_buffer(A1, A2, H=100, W=100, m=5):
+    '''
+    Computes closest points for every source pointcloud A1 using the z-buffer
+    '''
+
+    # Even m results in unclear window.
+    if m % 2 == 0:
+        raise ValueError("Value m={} invalid. m must be odd.".format(m))
+
+    # calculate floor of m / 2
+    m_flr = m // 2
+
+    # Take the union of A1, A2.
+    union = np.vstack((A1.T, A2.T))
+    union = union.T
+
+
+    # Determine the minimum enclosing box on the x,y-plane.
+    box = {"x_min": np.min(union[0]), "x_max": np.max(union[0]),
+           "y_min": np.min(union[1]), "y_max": np.max(union[1])}
+
+    width = box["x_max"] - box["x_min"]
+    w_step = width / W
+
+    height = box["y_max"] - box["y_min"]
+    h_step = height / H
+
+
+    # Divide minimum enclosing box into H×W rectangular cells and find centroid for each cell
+    cell_centroids = np.zeros((H*W, 2))
+    for i in range(H):
+        for j in range(W):
+            ind = i * H + j
+            cell_centroids[ind][0] = box['x_min'] + w_step/2 + j*w_step
+            cell_centroids[ind][1] = box['y_min'] + h_step/2 + i*h_step
+
+
+    # Calculate distance from each point to each centroid
+    kdTree = cKDTree(cell_centroids)
+    _, idx_source = kdTree.query(A1[:2].T)
+    _, idx_target = kdTree.query(A2[:2].T)
+
+
+    # Buffers with (HxW, 2) to store index and z-value repectively
+    source_buffer = np.zeros((H*W, 2))
+    for i, idx in enumerate(idx_source):
+        if np.abs(A1[2, i]) > np.abs(source_buffer[idx][1]):
+            source_buffer[idx][0] = i
+            source_buffer[idx][1] = A1[2, i]
+
+    target_buffer = np.zeros((H*W, 2))
+    for i, idx in enumerate(idx_target):
+        if np.abs(A2[2, i]) > np.abs(target_buffer[idx][1]):
+            target_buffer[idx][0] = i
+            target_buffer[idx][1] = A2[2, i]
+
+
+    # Find matches between source and target
+    matches = {'source': [], 'target': []}
+    for i in range(H*W):
+        closest = (i, np.inf)
+
+        # Save closest target cell in m*m area
+        for y in range(i - m_flr*W, i + m_flr*(W + 1), W):
+            for x in range(y - m_flr, y + m_flr + 1):
+
+                # Prevent x from indexing out-of-bounds.
+                if x < 0 or x > H*W - 1:
+                    continue
+                elif np.abs(target_buffer[x][1]) < np.abs(closest[1]):
+                    closest = (x, target_buffer[x][1])
+
+        matches['source'].append(i)
+        matches['target'].append(closest[0])
+
+
+    # Slice A1, A2 to obtain matching subsets.
+    new_A1 = A1[:, matches['source']]
+    matches = A2[:, matches['target']]
+
+    return new_A1, matches
 
 
 def calculate_RMS(source, target):
@@ -49,25 +132,25 @@ def get_new_R_t(source, target):
     #compute the centered vectors
     x = source.T - x_centroid
     y = target.T - y_centroid
-    
+
     #compute the covariance matix
     A = x.T @ y
 
     #compute the SVD and get R,t
-    U, S, Vt = np.linalg.svd(A)
+    U, _, Vt = np.linalg.svd(A)
 
     I = np.eye(Vt.shape[1])
     I[-1, -1] = 1/np.linalg.det(Vt.T @ U.T)
 
     R = Vt.T @ I @ U.T
-    
+
     t = y_centroid - R @ x_centroid
     t = np.expand_dims(t, axis=1)
 
     return R, t
 
 
-def icp(A1, A2, sampling='none', max_iters=50, epsilon=1e-4, ratio=0.1, kd_tree=False, N=4, alpha=0.3, noise=False, noise_max=0.1, print_rms=True):
+def icp(A1, A2, sampling='none', max_iters=50, epsilon=1e-4, ratio=0.1, mode='kd_tree', N=4, alpha=0.3, noise=False, noise_max=0.1, print_rms=True):
     '''
     Perform ICP algorithm to perform Rototranslation.
 
@@ -79,8 +162,8 @@ def icp(A1, A2, sampling='none', max_iters=50, epsilon=1e-4, ratio=0.1, kd_tree=
         - max_iters:   Max iterations the ICP can run.
         - epsilon:     Error threshold.
         - ratio:       Total % of points to use for sampling.
-        - kd_tree:     Determine whether kd_tree algorithm used for
-                       point matching.
+        - mode:        ['kd_tree', 'z_buffer']. Determine whether kd_tree algorithm
+                       or z_buffer is used for point matching.
         - N:           Determines step size of multi-res.
         - alpha:       Set the alpha value for the triangle mesh.
         - noise        Set whether noise is added.
@@ -104,11 +187,11 @@ def icp(A1, A2, sampling='none', max_iters=50, epsilon=1e-4, ratio=0.1, kd_tree=
 
     past_rms = np.inf
     all_rms = []
-  
+
     if sampling == 'uniform':
         new_A1 = subsample_graph(A1, points=int(A1.shape[1] * ratio))
         new_A2 = subsample_graph(A2, points=int(A2.shape[1] * ratio))
-        
+
     if sampling == 'info_reg':
         new_A1, new_A2 = obtain_informative_regions(A1, A2, alpha, int(A1.shape[1] * ratio))
 
@@ -119,8 +202,9 @@ def icp(A1, A2, sampling='none', max_iters=50, epsilon=1e-4, ratio=0.1, kd_tree=
         new_A2 = subsample_graph(A2, points=points_sampled[cur_step])
 
     for iter in range(max_iters):
-        
+
         # 2. transform point cloud with R and t
+
         A1 = R @ A1 + t
 
         # 3. Find the closest point for each point in A1 based on A2 using different approaches
@@ -133,7 +217,7 @@ def icp(A1, A2, sampling='none', max_iters=50, epsilon=1e-4, ratio=0.1, kd_tree=
         elif sampling == 'random':
             new_A1 = subsample_graph(A1, points=int(A1.shape[1] * ratio))
             new_A2 = subsample_graph(A2, points=int(A2.shape[1] * ratio))
-        
+
         # Multi resolution
         elif sampling == 'multi_res':
             new_A1 = R @ new_A1 + t
@@ -142,9 +226,13 @@ def icp(A1, A2, sampling='none', max_iters=50, epsilon=1e-4, ratio=0.1, kd_tree=
         else:
             new_A1 = A1
             new_A2 = A2
-        
-        if kd_tree:
+
+        # Use KDTree to find distances
+        if mode == 'kd_tree':
             matches = calculate_closest_points_kd_tree(new_A1, new_A2)
+        # Use Z-buffer to match points
+        elif mode == 'z_buffer':
+            new_A1, matches = calculate_closest_points_z_buffer(new_A1, new_A2)
         else:
             matches = calculate_closest_points(new_A1, new_A2)
 
@@ -154,19 +242,17 @@ def icp(A1, A2, sampling='none', max_iters=50, epsilon=1e-4, ratio=0.1, kd_tree=
 
         if print_rms:
             print('Iter', iter, 'RMS', rms)
-        if abs(past_rms-rms) < epsilon:
-            break
 
         # 5. Check if RMS is unchanged or within threshold
         if abs(past_rms-rms) < epsilon:
             break
-        
+
         past_rms_x = past_rms
         past_rms = rms
 
         # 6. Refine R and t using SVD
         R, t = get_new_R_t(new_A1, matches)
-        
+
         # Move in the sampling amount dependent on RMS for multi-res.
         if sampling == 'multi_res':
             # Increase points sampled
@@ -177,6 +263,7 @@ def icp(A1, A2, sampling='none', max_iters=50, epsilon=1e-4, ratio=0.1, kd_tree=
                     new_A2 = subsample_graph(A2, points=points_sampled[cur_step])
                 else:
                     new_A2 = subsample_graph(A2, points=A2.shape[1])
+
 
         # 7. Update rotation and translation
         rotation = R @ rotation
@@ -192,15 +279,11 @@ def icp(A1, A2, sampling='none', max_iters=50, epsilon=1e-4, ratio=0.1, kd_tree=
 
 if __name__ == "__main__":
 
-    # source, target = open_wave_data()
-    source, target = open_bunny_data()
-
-    # source = subsample_graph(source, points=int(source.shape[1] * 0.85))
-    # target = subsample_graph(target, points=int(source.shape[1] * 0.85))
-    
+    # source, target, save_dir = open_wave_data()
+    source, target, save_dir = open_bunny_data()
     samplings = ['uniform', 'random', 'multi_res', 'info_reg', 'none']
     time1 = time.time()
-    R, t, _ = icp(source, target, sampling=samplings[4], epsilon=1e-8, max_iters=50, ratio=0.1, kd_tree=False)
+    R, t, _ = icp(source, target, sampling='none', epsilon=1e-8, max_iters=50, ratio=0.1, mode='kd_tree')
     print("Time:", time.time() - time1)
     trans = (R @ source) + t
-    plot_progress(source, target, trans, dir='./figures/wave.png', save_figure=False)
+    plot_progress(source, target, trans, dir=save_dir, save_figure=False)
