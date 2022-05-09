26fbb007cddb57cb8fc12f40cc79fb594197db5e
MAStrampel <59614509+MAStrampel@users.noreply.github.com>
2022-05-09 19:57:09 +0200

    Added code for section 5

--- assignment2/Code/pointview_matrix.py
+++ assignment2/Code/pointview_matrix.py
@@ -1,14 +1,59 @@
 from fundamental_matrix import *
 import cv2 as cv
 import numpy as np
+from scipy import spatial
 
 
 def shared_points(mtx):
     '''
-    Obtain the sparse Point-view matrix.
+    Obtain the dense Point-view matrix.
     '''
     return mtx[:, np.all(mtx != -1.0, axis=0)]
 
+def compute_structures(pvm, frame_step=3):
+    structures, idxs = [], []
+    for i in range(0, len(pvm), frame_step*2):
+
+        # Select xy pairs for frame_step frames.
+        block = pvm[i:i+frame_step*2, :]
+
+        p = np.all(block != -1.0, axis=0)
+        block = block[:, p]
+        idx = np.where(p)
+
+        # Remove all colums that contain at least one -1 entry.
+        # block = block[:, np.all(block != -1.0, axis=0)]
+
+        # Do not consider blocks that are < 3 frames or < 3 points per frame.
+        if block.shape[0] != frame_step*2 or block.shape[1] < frame_step:
+            continue
+
+        idxs.append(idx)
+
+        # Normalise block.
+        block = block - np.expand_dims(np.mean(block, axis=1), axis=1)
+
+        S = find_structure_motion(block)
+        structures.append(S)
+
+    return structures, idxs
+
+def find_structure_motion(block):
+    U, W, VT = np.linalg.svd(block)
+
+    # Take three columns, values, rows respectively.
+    U = U[:, :3]
+    W = np.diag(W[:3])
+    VT = VT[:3, :]
+
+    # Calculate Motion and Structure matrices.
+    M = U @ np.sqrt(W)
+    S = np.sqrt(W) @ VT
+
+    # Check if rank of D = M @ S is equal to 3, which it should always be.
+    assert np.linalg.matrix_rank(M @ S) == 3
+
+    return S
 
 def construct_new_pv_row(X1, Y1, X2, Y2, cur_x, cur_y):
     '''
@@ -57,6 +102,7 @@ def update_columns(pv_mtx, new_cols):
     for row in pv_mtx:
         new_row = np.append(row, zeroes)
         new_pv_mtx.append(new_row)
+
     return new_pv_mtx
 
 
@@ -66,8 +112,8 @@ def create_pv_matrix(start=1, N=50, dist_ratio=0.2):
     '''
     pv_mtx = []
 
-    img1 = cv.imread(f'./Data/House/frame000000{"{0:02}".format(start)}.png')
-    img2 = cv.imread(f'./Data/House/frame000000{"{0:02}".format(start+1)}.png')
+    img1 = cv.imread(f'../Data/house/frame000000{"{0:02}".format(start)}.png')
+    img2 = cv.imread(f'../Data/house/frame000000{"{0:02}".format(start+1)}.png')
 
     points1, points2 = calculate_keypoint_matching(img1, img2, dist_ratio, draw=False)
 
@@ -77,8 +123,8 @@ def create_pv_matrix(start=1, N=50, dist_ratio=0.2):
     pv_mtx.append(points2[:, 1])
 
     for i in range(start + 1, N - 1):
-        img1 = cv.imread(f'./Data/House/frame000000{"{0:02}".format(i)}.png')
-        img2 = cv.imread(f'./Data/House/frame000000{"{0:02}".format(i+1)}.png')
+        img1 = cv.imread(f'../Data/house/frame000000{"{0:02}".format(i)}.png')
+        img2 = cv.imread(f'../Data/house/frame000000{"{0:02}".format(i+1)}.png')
 
         # Keypoint matching
         points1, points2 = calculate_keypoint_matching(img1, img2, dist_ratio, draw=False)
@@ -109,7 +155,11 @@ def create_pv_matrix(start=1, N=50, dist_ratio=0.2):
 
 if __name__ == '__main__':
     mtx = create_pv_matrix()
-    print()
     print(mtx.shape)
-    mtx2 = shared_points(mtx)
-    print(mtx2.shape)
+    # mtx2 = shared_points(mtx)
+    # print(mtx2.shape)
+    # print(mtx2)
+    structs = compute_structures(mtx)
+    print(len(structs))
+    lens = [s.shape for s in structs]
+    print(lens)
