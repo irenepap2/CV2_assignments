a9c6f29d5cfc81f590cf557a5790b3252cbd5b5e
Gijsvanmeer <59614500+Gijsvanmeer@users.noreply.github.com>
2022-05-13 14:17:01 +0200

    Added density improvement to the PVM

--- assignment2/Code/pointview_matrix.py
+++ assignment2/Code/pointview_matrix.py
@@ -11,7 +11,47 @@ def shared_points(mtx):
     return mtx[:, np.all(mtx != -1.0, axis=0)]
 
 
-def construct_new_pv_row(X1, Y1, X2, Y2, cur_x, cur_y):
+def improve_density(cur_x, cur_y, dist_ratio, pv_mtx, start=1, index=2):
+    '''
+    Find existing matches with all previous frames.
+    '''
+    for i in range(start + 1, index-1):
+        img1 = cv.imread(f'./Data/house/frame000000{"{0:02}".format(i)}.png')
+        img2 = cv.imread(f'./Data/house/frame000000{"{0:02}".format(index)}.png')
+        points1, points2 = calculate_keypoint_matching(img1, img2, dist_ratio, draw=False)
+
+        # Obtain the row with the already obtained matches.
+        m_x = pv_mtx[2*(i-2)]
+        m_y = pv_mtx[2*(i-2) + 1]
+
+        # Only continue if we find points.
+        if not isinstance(points1, list):
+
+            # Obtain the rows of the new matches.
+            X1 = points1[:, 0]
+            Y1 = points1[:, 1]
+            X2 = points2[:, 0]
+            Y2 = points2[:, 1]
+
+            # Add the new points if they exist
+            for j in range(len(X1)):
+                for m in range(len(m_x)):
+                    if m_x[m] == X1[j] and m_y[m] == Y1[j]:
+                        if cur_x[m] == -1:
+                            add_p = True
+                            for x, y in zip(cur_x, cur_y):
+                                # If a point has already been added, dont add it again
+                                if x == X2[j] and y == Y2[j]:
+                                    add_p = False
+                                    break
+                            if add_p:
+                                cur_x[m] = X2[j]
+                                cur_y[m] = Y2[j]
+                        break
+    return cur_x, cur_y
+
+
+def construct_new_pv_row(X1, Y1, X2, Y2, cur_x, cur_y, dist_ratio, pv_mtx, start, index, improv=False):
     '''
     Find matches between Point-view matrix rows and add new column when
     new point found.
@@ -45,6 +85,9 @@ def construct_new_pv_row(X1, Y1, X2, Y2, cur_x, cur_y):
             cur_x = np.insert(cur_x, arr_len, X1[j])
             cur_y = np.insert(cur_y, arr_len, Y1[j])
 
+    if improv:
+        cur_x, cur_y = improve_density(cur_x, cur_y, dist_ratio, pv_mtx, start, index)
+
     return cur_x, cur_y, X2_new, Y2_new
 
 
@@ -62,7 +105,7 @@ def update_columns(pv_mtx, new_cols):
     return new_pv_mtx
 
 
-def create_pv_matrix(start=1, N=50, dist_ratio=0.7):
+def create_pv_matrix(start=1, N=50, dist_ratio=0.7, improv=False):
     '''
     Create Point-view matrix with images start to N.
     '''
@@ -84,7 +127,7 @@ def create_pv_matrix(start=1, N=50, dist_ratio=0.7):
             img2 = cv.imread(f'./Data/house/frame000000{"{0:02}".format(start)}.png')
         else:
             img2 = cv.imread(f'./Data/house/frame000000{"{0:02}".format(i+1)}.png')
-        
+
         # Keypoint matching
         points1, points2 = calculate_keypoint_matching(img1, img2, dist_ratio, draw=False)
         if len(points1) == 0:
@@ -101,7 +144,7 @@ def create_pv_matrix(start=1, N=50, dist_ratio=0.7):
         Y2 = points2[:, 1]
 
         # Obtain the new rows.
-        new_x, new_y, new_X2, new_Y2 = construct_new_pv_row(X1, Y1, X2, Y2, cur_x, cur_y)
+        new_x, new_y, new_X2, new_Y2 = construct_new_pv_row(X1, Y1, X2, Y2, cur_x, cur_y, dist_ratio, pv_mtx, start, i+1, improv=improv)
 
         # If new columns were added update the Point-view matrix.
         if len(new_x) != len(cur_x):
@@ -117,15 +160,21 @@ def create_pv_matrix(start=1, N=50, dist_ratio=0.7):
 def visualize_pvm(pvm):
     pvm[pvm!=-1] = 0
     pvm[pvm==-1] = 1
+    plt.ylabel('Frames (2N)')
+    plt.xlabel('Points')
     plt.imshow(pvm, cmap='gray', aspect=20)
     plt.show()
 
 
 if __name__ == '__main__':
 
-    # pvm = np.loadtxt('PVM_ours_last.txt')
+    # pvm = np.loadtxt('PVM_ours.txt')
     # pvm = np.loadtxt('PointViewMatrix.txt')
-    pvm = create_pv_matrix()
-    # np.savetxt('PVM_ours_last.txt', pvm)
+    pvm = create_pv_matrix(improv=True)
+    # with open('PVM_ours_improv.txt', 'w') as f:
+    #     for row in pvm:
+    #         f.write(' '.join([str(item) for item in row]))
+    #         f.write('\n')
     print(pvm.shape)
     visualize_pvm(pvm.copy())
+    print(shared_points(pvm).shape)
